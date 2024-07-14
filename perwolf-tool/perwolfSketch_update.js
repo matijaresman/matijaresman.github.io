bab02da06c45030a8ec266f640cac0dcfed029f4
matijaresman <134400027+matijaresman@users.noreply.github.com>
2024-07-14 21:32:59 +0200

    Update perwolfSketch_update.js

--- perwolf-tool/perwolfSketch_update.js
+++ perwolf-tool/perwolfSketch_update.js
@@ -174,3 +174,24 @@ document.addEventListener("DOMContentLoaded", function() {
         
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const openPopupBtn = document.getElementById('openPopupBtn');
+    const popup = document.getElementById('popup');
+    const closePopupBtn = document.getElementById('closePopupBtn');
+
+    openPopupBtn.addEventListener('click', function() {
+        popup.style.display = 'flex';
+    });
+
+    closePopupBtn.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+
+    // Close the popup when clicking outside of it
+    window.addEventListener('click', function(event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
